dcbe67744aa2dc89acecbb995106c3fffd01b669
Thenujan Sandramohan <sthenujan2002@gmail.com>
2023-05-01 17:53:50 +0530

    Improve SelectTags component
    After this MR click events outside selectable tags do not close the selectTags popup

--- resources/js/components/SelectTags.jsx
+++ resources/js/components/SelectTags.jsx
@@ -1,7 +1,7 @@
 import PopupMenu from "./PopupMenu"
 import GridView from "./GridView"
 import TagItem from "./TagItem"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import axios from "axios"
 
 
@@ -10,6 +10,21 @@ function SelectTags({style, tags, confirmCallbackParent}){
 
     const [selectedTags, selectedTagsUpdate] = useState([])
     const deSelectedTags = tags.filter((elem)=>!selectedTags.includes(elem))
+
+    function removeFromSelectedTags(tag){
+            selectedTagsUpdate((selectedTags)=>{
+                return selectedTags.filter((elem)=>{
+                    return elem != tag
+                })
+            })
+    }
+
+    function addToSelectedTags(tag){
+        selectedTagsUpdate((selectedTags)=>{
+            return [...selectedTags,tag]
+        })
+    }
+
     function clickCallback(e){
         let target = e.target
         if(target instanceof HTMLHeadingElement){
@@ -22,13 +37,17 @@ function SelectTags({style, tags, confirmCallbackParent}){
 
         for(let i=0;i<tags.length;i++){
             const tag = tags[i]
-            if (tag.id == id){
+            if (tag.id != id){
+                continue
+            }
 
-                selectedTagsUpdate((selectedTags)=>{
-                    return [...selectedTags,tag]
-                })
+            if (selectedTags.includes(tag)){
+                removeFromSelectedTags(tag)
                 break
             }
+
+            addToSelectedTags(tag)
+            break
         }
 
     }
@@ -44,17 +63,31 @@ function SelectTags({style, tags, confirmCallbackParent}){
         confirmCallbackParent()
         selectedTagsUpdate([])
     }
+    const selectTagElement = useRef(null)
+    useEffect(()=>{
+        document.addEventListener('click',(e)=>{
+            if (selectTagElement.current.contains(e.target)){
+                e.stopImmediatePropagation()
+            }
+        })
+    },[])
 
     return (<>
-        <PopupMenu style={{...style,maxWidth:'50%'}}>
-            <h5 className="text-center">Selected Tags</h5>
-            <GridView>
-                {selectedTags.map((tag)=>{return <TagItem key={tag.id} name={tag.name}></TagItem>})}
-            </GridView>
-            <h5 className="text-center">Select tags to add</h5>
-            <GridView>
-                {deSelectedTags.map((tag)=>{return <TagItem key={tag.id} id={tag.id} onClick={clickCallback} name={tag.name}/>})}
-            </GridView>
+        <PopupMenu style={{...style,maxWidth:'50%'}} ref={selectTagElement}>
+            <div className="selected-tags bg-secondary rounded p-2 mb-4" >
+                <h5 className="text-center text-white">Selected Tags</h5>
+                <GridView style={{minHeight:"50px"}} >
+                    {selectedTags.map((tag)=>{return <TagItem key={tag.id} id={tag.id} onClick={clickCallback} name={tag.name}></TagItem>})}
+                </GridView>    
+            </div>
+            
+            <div className="selectable-tags bg-secondary rounded p-2 mb-4">
+                <h5 className="text-center text-white">Select tags to add</h5>
+                <GridView style={{minHeight:"50px"}} >
+                    {deSelectedTags.map((tag)=>{return <TagItem key={tag.id} id={tag.id} onClick={clickCallback} name={tag.name}/>})}
+                </GridView>
+            </div>
+
 
             <button className="btn btn-primary" onClick={confirmCallback}>Confirm</button>
         </PopupMenu>
