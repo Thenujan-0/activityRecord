9a63c7040fa128f0bd296832e313e6a7b5a2eb26
Thenujan Sandramohan <sthenujan2002@gmail.com>
2023-05-07 00:59:32 +0530

    Switch to FullCalendar as Root component

--- resources/js/components/Root.jsx
+++ resources/js/components/Root.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Calendar from './Calendar';
+import FullCalendar from './FullCalendar';
 
 if (document.getElementById('root')) {
     const Index = ReactDOM.createRoot(document.getElementById("root"));
     Index.render(
         <React.StrictMode>
             <div className="container d-flex align-items-center justify-content-center">
-                <Calendar year="2023" month="4"/>
+                <FullCalendar year="2023" month="4"/>
             </div>
         </React.StrictMode>
     )
